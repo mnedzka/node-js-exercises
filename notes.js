3124a8e10df7e4a7f73a70bf50f610eec277d770
Michal Nedzka <michael.nedzka@gmail.com>
2018-12-05 23:29:34 +0100

    Refactor add function and add remove function

--- notes.js
+++ notes.js
@@ -2,25 +2,32 @@ console.log("Starting notes.js");
 
 const fs = require("fs");
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync("notes-data.json");
+    return (notes = JSON.parse(notesString));
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = notes => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title,
     body
   };
 
-  try {
-    var notesString = fs.readFileSync("notes-data.json");
-    notes = JSON.parse(notesString);
-  } catch (e) {
-    // console.log(e);
-  }
-
   var duplicateNotes = notes.filter(note => note.title === title);
 
   if (duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
 };
 
@@ -33,7 +40,11 @@ var getNote = title => {
 };
 
 var removeNote = title => {
-  console.log("Removing note", title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter(note => note.title !== title);
+  saveNotes(filteredNotes);
+
+  return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
